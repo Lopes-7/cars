1aef8a9b221d714c0b47f3eeed61d42fd3e83be4
Lopes-7 <gui-lopes7@live.com>
2020-07-22 15:14:30 -0300

    FIXUP Add Sidebar Header Component

--- src/components/sidebarheader.js
+++ src/components/sidebarheader.js
@@ -1,7 +1,6 @@
 /**
  * IMPORTS
- */
-import {Component} from 'react';  
+ */ 
 import React from 'react';
 import {Icon} from '@blueprintjs/core';
 import {IconNames} from '@blueprintjs/icons';
@@ -16,13 +15,17 @@ import '../styles/sidebarheader.css';
 /**
  * CODE
  */
-export class SidebarHeader extends Component{
-    render() { 
-        return ( 
-            <div className='sidebarheader'>
-                <Icon className='icon' icon={IconNames.DRIVE_TIME}></Icon>
-                <h1 className='title'>Registrar novo carro</h1>
-            </div>
-         );
-    }
+function SidebarHeader() {
+    return ( 
+        <div className='sidebarheader'>
+            <Icon className='icon' icon={IconNames.DRIVE_TIME}></Icon>
+            <h1 className='title'>Registrar novo carro</h1>
+        </div>
+    );
 }
+
+
+/**
+ * EXPORTS
+ */
+export {SidebarHeader};
